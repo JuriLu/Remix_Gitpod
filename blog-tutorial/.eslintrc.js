5cd1361ceba8c58a98ecad15ec1fd9d14ec9ef65
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-12-21 14:00:26 +0000

    api Logic moved to model + DB update for creating Post

--- blog-tutorial/.eslintrc.js
+++ blog-tutorial/.eslintrc.js
@@ -18,4 +18,124 @@ module.exports = {
       version: 28,
     },
   },
+    "root": true,
+    "ignorePatterns": [
+      "**/*"
+    ],
+    "plugins": [
+      "@nrwl/nx"
+    ],
+    "overrides": [
+      {
+        "files": [
+          "*.ts",
+          "*.tsx",
+          "*.js",
+          "*.jsx"
+        ],
+        "rules": {
+          //        "@nrwl/nx/enforce-module-boundaries": [
+          //          "error",
+          //          {
+          //            "enforceBuildableLibDependency": true,
+          //            "allow": [],
+          //            "depConstraints": [
+          //              {
+          //                "sourceTag": "*",
+          //                "onlyDependOnLibsWithTags": [
+          //                  "*"
+          //                ]
+          //              }
+          //            ]
+          //          }
+          //        ]
+        }
+      },
+      {
+        "files": [
+          "*.ts",
+          "*.tsx"
+        ],
+        "extends": [
+          "plugin:@nrwl/nx/typescript"
+        ],
+        "rules": {
+          "quotes": [
+            "error",
+            "single",
+            {
+              "allowTemplateLiterals": true
+            }
+          ],
+          "@typescript-eslint/no-empty-function": [
+            "error",
+            {
+              "allow": [
+                "methods",
+                "constructors"
+              ]
+            }
+          ],
+          "no-var": [
+            "error"
+          ],
+          "no-console": [
+            "error"
+          ],
+          "no-duplicate-imports": "error",
+          "indent": [
+            "error",
+            2,
+            {
+              "SwitchCase": 1
+            }
+          ],
+          "max-len": [
+            "error",
+            140
+          ],
+          "semi": "error",
+          "space-before-function-paren": [
+            "error",
+            {
+              "anonymous": "never",
+              "named": "never",
+              "asyncArrow": "always"
+            }
+          ],
+          "consistent-return": [
+            "error"
+          ],
+          "@typescript-eslint/explicit-function-return-type": [
+            "error",
+            {
+              "allowConciseArrowFunctionExpressionsStartingWithVoid": false,
+              "allowExpressions": true
+            }
+          ],
+          "no-restricted-imports": [
+            "error",
+            {
+              "paths": [
+                "rxjs/Rx"
+              ]
+            }
+          ],
+          "curly": "error",
+          "no-trailing-spaces": "error"
+        }
+      },
+      {
+        "files": [
+          "*.js",
+          "*.jsx"
+        ],
+        "extends": [
+          "plugin:@nrwl/nx/javascript"
+        ],
+        "rules": {}
+      }
+    ]
+
 };
+
